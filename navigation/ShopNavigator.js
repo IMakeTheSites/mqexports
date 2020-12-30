ebce7c7bb44be348ad7926a095debae7efef0def
Mark Wlodawski <markwlodawski@marks-mbp-2.lan>
2020-12-30 12:40:14 -0500

    User authentication Login Screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
@@ -11,6 +11,7 @@ import EditProductScreen from "../screens/user/EditProductScreen";
 import Colors from "../constants/Colors";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import AuthScreen from "../screens/user/AuthScreen";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -95,4 +96,18 @@ const ShopNavigator = createDrawerNavigator(
   }
 );
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator(
+  {
+    Auth: AuthScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator,
+});
+
+export default createAppContainer(MainNavigator);
---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Button,
+} from "react-native";
+import Input from "../../components/UI/Input";
+import Card from "../../components/UI/Card";
+import Colors from "../../constants/Colors";
+import { LinearGradient } from "expo-linear-gradient";
+
+const AuthScreen = (props) => {
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={50}
+      style={styles.screen}
+    >
+      <LinearGradient
+        colors={[Colors.primary, Colors.accent]}
+        style={styles.linearGradient}
+      >
+        <Card style={styles.authContainer}>
+          <ScrollView>
+            <Input
+              id="email"
+              label="E-mail"
+              keyboardType="email-address"
+              required
+              email
+              autoCapitalize="none"
+              errorMessage="Please enter a valid email address"
+              onInputChange={() => {}}
+              initialValue=""
+            />
+            <Input
+              id="password"
+              label="Password"
+              keyboardType="default"
+              secureTextEntry
+              required
+              minLength={5}
+              autoCapitalize="none"
+              errorMessage="Please enter a valid password"
+              onInputChange={() => {}}
+              initialValue=""
+            />
+            <View style={styles.buttonContainer}>
+              <Button title="Login" color={Colors.primary} onPress={() => {}} />
+            </View>
+            <View>
+              <Button
+                title="Switch to Sign Up"
+                color={Colors.accent}
+                onPress={() => {}}
+              />
+            </View>
+          </ScrollView>
+        </Card>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
+
+AuthScreen.navigationOptions = {
+  headerTitle: "Authenticate",
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  authContainer: {
+    width: "80%",
+    maxWidth: 400,
+    maxHeight: 400,
+    padding: 10,
+  },
+  linearGradient: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+});
+
+export default AuthScreen;
